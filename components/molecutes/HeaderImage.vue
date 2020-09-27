2cf3841f475d170a5faa6b9bda6b4a478ede9e92
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-09-27 22:21:10 +0900

    _idに写真埋め込み

---
+++ components/molecutes/HeaderImage.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container >
+        <v-row justify="center">
+            <img src="~/assets/test.jpg" class="img"/>
+        </v-row>
+    </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    name:'HeaderImage',
+})
+
+</script>
+
+<style>
+.img{
+    text-align:center
+}
+</style>
---
+++ pages/recipes/sample.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+        <HeaderImage :src="src"/>
+    </v-container>
+</template>
+
+<script>
+import Vue from 'vue'
+import HeaderImage from '~/components/molecutes/HeaderImage'
+
+export default Vue.extend({
+    components:{
+        HeaderImage
+    },
+
+    data(){
+        return {
+            src: '~/assets/test.jpg'
+        }
+    }
+})
+</script>
