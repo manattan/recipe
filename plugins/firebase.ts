215f0fdfb926fa16ec3553df410c35947ecbf365
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-10-01 16:36:28 +0900

    firebaseと連携

---
+++ plugins/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+if(!firebase.app.length) {
+    firebase.initializeApp(
+        {
+            apiKey: 'AIzaSyD430U_PzBaaI6WSMKh9PX6-VlOeAUfuSc',
+            authDomain:'recipe-ab8e5.firebaseapp.com',
+            databaseURL: 'https://recipe-ab8e5.firebaseio.com/',
+            projectedId: 'recipe-ab8e5',
+            storageBucket: 'recipe-ab8e5.appspot.com',
+            messagingSenderId: '864930084747',
+            appId: '1:864930084747:web:e3d3abf66145e8cac9618e'
+        }
+    )
+}
+
+export default firebase
