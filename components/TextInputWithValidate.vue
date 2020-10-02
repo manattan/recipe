6f51f9d51c781f741d7745c2b6cae0ea6c5f9e48
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-10-02 16:01:05 +0900

    validateコンポーネント実装

---
+++ components/TextInputWithValidate.vue
@@ -0,0 +1,50 @@
+<template>
+    <ValidationProvider v-slot="{errors}" :rules="rules">
+        <v-text-field
+            v-model="newValue"
+            :error-messages="errors"
+            :label="label"
+            v-bind="$attrs"
+        />
+    </ValidationProvider>
+</template>
+
+<script>
+import Vue from 'vue'
+import {ValidationProvider} from 'vee-validate'
+
+export default Vue.extend({
+    name:'TextInputWithValidate',
+
+    components:{
+        ValidationProvider
+    },
+    props: {
+        value:{
+            type: String,
+            required:true
+        },
+        label: {
+            type: String,
+            required:true
+        },
+        rules: {
+            type: String,
+            required: false
+        },
+    },
+
+    computed : {
+        newValue: {
+            get(){
+                return this.value
+            },
+            set(val) {
+                if (this.value !== val) {
+                this.$emit('input', val)
+                }
+            }
+        }
+    }
+})
+</script>
