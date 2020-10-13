eff15502bc2fcfcbc497643223ac66f12664dfbe
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-10-13 14:07:53 +0900

    index.vue完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
     <v-container>
-
+        <v-row>
+            <v-col v-for="(recipe,i)in allRecipe" :key="i">
+                <RecipeCard :src="recipe.url" :recipe="recipe.recipe" :id="recipe.id"/>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { getAllRecipeData } from '~/utils/firestore'
+import firebase from '~/plugins/firebase'
+import { storage } from 'firebase'
 
 export default Vue.extend({
     data(){
@@ -16,8 +22,14 @@ export default Vue.extend({
     },
 
     async created(){
-        this.allRecipe= await getAllRecipeData()
-        
+        const res= await getAllRecipeData()
+        for (const tmp of res){
+            var storageRef = firebase.storage().ref()
+            const url=storageRef.child(`Recipes/${tmp.id}.img`).getDownloadURL()
+            this.allRecipe.push(Object.assign(tmp,{url:url}))
+
+        }
+
     }
 })
 </script>
