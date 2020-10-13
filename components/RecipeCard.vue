7b3412433993d4d0dab34677087af45ca695744b
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-10-13 14:34:36 +0900

    とりあえず表示だけはできる

---
+++ components/RecipeCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <section>
+        <v-img :src="url" />
+    </section>
+</template>
+
+<script lang="ts">
+import Vue,{PropType} from 'vue'
+import * as commonTypes from '~/types/common'
+import firebase from '~/plugins/firebase'
+
+export default Vue.extend({
+    props:{
+        url:{
+            type:String,
+            required:true,
+        },
+        title:{
+            type:String,
+            required:true
+        },
+        summary:{
+            type:String,
+            required:true
+        },
+        userName:{
+            type:String,
+            required:true
+        },
+        updateTimestamp:{
+            type:Object,
+            required:true
+        },
+        id:{
+            type:String,
+            required:true,
+        },
+    }
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
     <v-container>
         <v-row>
-            <v-col v-for="(recipe,i)in allRecipe" :key="i">
-                <RecipeCard :src="recipe.url" :recipe="recipe.recipe" :id="recipe.id"/>
+            <v-col v-for="(recipe,i) in allRecipe" :key="i">
+                <RecipeCard
+                    :url="recipe.url"
+                    :title="recipe.name"
+                    :summary="recipe.summary"
+                    :updateTimestamp="recipe.updateTimestamp"
+                    :userName="recipe.userName"
+                    :id="recipe.id"
+                />
             </v-col>
         </v-row>
     </v-container>
@@ -10,11 +17,15 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import RecipeCard from '~/components/RecipeCard.vue'
 import { getAllRecipeData } from '~/utils/firestore'
 import firebase from '~/plugins/firebase'
 import { storage } from 'firebase'
 
 export default Vue.extend({
+    components:{
+        RecipeCard
+    },
     data(){
         return {
             allRecipe:[] as firebase.firestore.DocumentData & {id: string }[]
@@ -24,12 +35,14 @@ export default Vue.extend({
     async created(){
         const res= await getAllRecipeData()
         for (const tmp of res){
+            console.log("storageに接続開始")
             var storageRef = firebase.storage().ref()
-            const url=storageRef.child(`Recipes/${tmp.id}.img`).getDownloadURL()
+            const url=await storageRef.child(`Recipes/${tmp.id}.jpg`).getDownloadURL()
             this.allRecipe.push(Object.assign(tmp,{url:url}))
+            console.log(this.allRecipe)
+            console.log("urlをassign")
 
         }
-
     }
 })
 </script>
